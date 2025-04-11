a465ff6c87e976b776d14cfb47620c399a828d47
SebasLzCb <sebas2006loza@gmail.com>
2025-04-10 19:15:53 -0500

    Cambios y agregacion de nuevos metodos

--- src/App.java
+++ src/App.java
@@ -1,14 +1,17 @@
 public class App {
      public static void main(String[] args) throws Exception {
         EjerciciosRecursivos e = new EjerciciosRecursivos();
+
         int r = e.sumaConsecutivos(5);
         System.out.println(r);
         
         int fibo = e.fibonacci(5);
         System.out.println("Respuesta es " + fibo);
 
-
-
+        int potencia = e.getPotencia(2, 3);
+        System.out.println("2^3 es: " + potencia);
         
+        int suma = e.sumaDigitos(125);
+        System.out.println("Resultado: " + suma);
      }
 }
--- src/EjerciciosRecursivos.java
+++ src/EjerciciosRecursivos.java
@@ -25,5 +25,35 @@ public class EjerciciosRecursivos {
         int resultado = n + resultadoParcial;
         return resultado;
     }
+
+    /**
+     * Escribe una funcion recursiva que calcue la potencia
+     * de un numero base elevado a un exponente.
+     * Por ejemplo, si base es 2 y exponente es 3.
+     * La funcion debe devolver 2elevado a 3 = 8
+     */
+    public int getPotencia(int base, int exponente) {
+        if (exponente == 0) {
+            return 1;
+        }
+        return base * getPotencia(base, exponente - 1);
+    }
+
+    /**
+     * SUMA DE DIGITOS DE UN NUMERO 
+     * si el numero es menor que 10, lo cual significa que solo tiene
+     * un digito, se devuelve el numero mismo. En caso contrario, se extrae el ultimo digito utilizando el operador
+     * modulo % y se calcula los digitos menos el ultimo utilizando la division entera /10. Luego, se realiza una llamada 
+     * recursiva a sumaDigitos con el resto y se suma el ultimo digito
+     */
+    public int sumaDigitos(int numero){
+        if (numero < 10){
+            return numero;
+        }
+        int resto = numero % 10;
+        int digitos = numero / 10;
+        return resto + sumaDigitos(digitos); 
+    }
+    
 }
 
